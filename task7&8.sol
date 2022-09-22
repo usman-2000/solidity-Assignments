10d4a1ecec50bb43f7460d7f1d8d0c67af0b6899
Usman Rahim <80035765+usman-2000@users.noreply.github.com>
2022-09-22 21:10:00 +0500

    Update task7&8.sol

--- task7&8.sol
+++ task7&8.sol
@@ -27,3 +27,37 @@ contract task8{
         return series;
     }
 }
+
+
+//Practice
+
+// Task
+
+// Create a function digitSum(int n). Where n>0.
+// Return the sum of digit for n.
+// For Example - If n=345 then digitSum() must return 12 i.e. 3+4+5 = 12
+// If n=909 then digitSum() must return 18 i.e. 9+0+9 = 18
+
+// Note - Function will be public.
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract sumDigits{
+
+    function DigitSum(uint a) public pure returns(uint){
+        
+        uint sum = 0;
+        while(a > 0){
+
+            uint b = a%10;
+
+            sum += b;
+
+            a= a/10;
+            }
+            return(sum);
+
+
+    }
+}
